52d71572cba93071d97215ca5efa99e29db3bd73
David Pedrero <pedrero888@gmail.com>
2023-10-24 21:11:20 -0400

    test connection 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js &&& Supabase',
+  title: 'Next.js & Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
 }
 
 export default function RootLayout({
